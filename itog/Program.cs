d6e9dacff6b4d7c88b4443055ecf0cd5c8834a7d
Svetlana <121864210+lana17121977@users.noreply.github.com>
2023-02-12 18:21:10 +0300

    ввели массив

---
+++ itog/Program.cs
@@ -0,0 +1,38 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+
+string[] arr1 = {"Hello", "2", "world", ":-)"};
+string[] arr2 = {"1234", "1567", "-2", "computer science"};
+string[] arr3 = {"Russia", "Denmark", "Kazan"};
+		
+int MAX_WORD_LENGTH = 3;
+		
+string[] GetShortWordsArray(string[] array)
+{
+	int length = array.Length;			
+	string[] result = new string[length];
+	int count = 0;
+			
+	for (int i = 0; i < length; i++)
+	{
+		if(array[i].Length <= MAX_WORD_LENGTH)
+		{
+			result[count] = array[i];
+			count++;
+		}
+	}
+	Array.Resize(ref result, count);
+			
+	return result;
+}
+		
+
+
+		
